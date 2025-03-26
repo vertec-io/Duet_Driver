bb85bd58c56754c0d8b8d4e7c7816d0cd80ec112
Luke Malcom <lmalcom@vertec.io>
2025-03-26 15:42:56 -0500

    feat: added lib file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-pub mod duet_driver;
-use duet_driver::DuetDriver;
+use duet_driver_app::DuetDriver;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -7,8 +6,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // FIXME: this needs to come from a .env file 
     let driver = DuetDriver::new("10.21.0.151".to_string()).await;
 
+    driver.send_position(50.0).await?;
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     driver.send_position(150.0).await?;
-    // Give the async loop a moment to process the command.
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    driver.send_position(50.0).await?;
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     Ok(())
 }
